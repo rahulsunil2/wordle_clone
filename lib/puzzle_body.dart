d0b97be162d7e1aa7f1651c54860e3af637b986c
Rahul Sunil <rahulsunil2@gmail.com>
2022-02-03 02:45:10 +0530

    Appbar Done

---
+++ lib/puzzle_body.dart
@@ -0,0 +1,154 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+class PuzzleBody extends StatelessWidget {
+  const PuzzleBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PinCodeVerificationScreen("+8801376221100");
+  }
+}
+
+class PinCodeVerificationScreen extends StatefulWidget {
+  final String? phoneNumber;
+
+  PinCodeVerificationScreen(this.phoneNumber);
+
+  @override
+  _PinCodeVerificationScreenState createState() =>
+      _PinCodeVerificationScreenState();
+}
+
+class _PinCodeVerificationScreenState extends State<PinCodeVerificationScreen> {
+  TextEditingController textEditingController = TextEditingController();
+
+  // ignore: close_sinks
+  StreamController<ErrorAnimationType>? errorController;
+
+  bool hasError = false;
+  String currentText = "HEL";
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    errorController = StreamController<ErrorAnimationType>();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    errorController!.close();
+
+    super.dispose();
+  }
+
+  // snackBar Widget
+  snackBar(String? message) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message!),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: GestureDetector(
+        onTap: () {},
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: ListView(
+            children: <Widget>[
+              Form(
+                key: formKey,
+                child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 32.0, horizontal: 30),
+                    child: PinCodeTextField(
+                      textCapitalization: TextCapitalization.characters,
+                      appContext: context,
+                      textStyle: const TextStyle(color: Color(0xffd8d7d5)),
+                      length: 5,
+                      animationType: AnimationType.scale,
+                      pinTheme: PinTheme(
+                        shape: PinCodeFieldShape.box,
+                        borderRadius: BorderRadius.circular(5),
+                        fieldHeight: 50,
+                        fieldWidth: 50,
+                        activeFillColor: Colors.transparent,
+                        borderWidth: 1,
+                      ),
+                      cursorColor: Colors.black,
+                      animationDuration: const Duration(milliseconds: 300),
+                      enableActiveFill: false,
+                      errorAnimationController: errorController,
+                      controller: textEditingController,
+                      keyboardType: TextInputType.text,
+                      onCompleted: (v) {
+                        print("Completed");
+                      },
+                      // onTap: () {
+                      //   print("Pressed");
+                      // },
+                      onChanged: (value) {
+                        print(value);
+                        setState(() {
+                          currentText = value;
+                        });
+                      },
+                      beforeTextPaste: (text) {
+                        print("Allowing to paste $text");
+                        //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                        //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                        return true;
+                      },
+                    )),
+              ),
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 30),
+                child: ButtonTheme(
+                  height: 50,
+                  child: TextButton(
+                    onPressed: () {
+                      formKey.currentState!.validate();
+                      // conditions for validating
+                      if (currentText.length != 5 || currentText != "123456") {
+                        errorController!.add(ErrorAnimationType
+                            .shake); // Triggering error shake animation
+                        setState(() => hasError = true);
+                      } else {
+                        setState(
+                          () {
+                            hasError = false;
+                            snackBar("OTP Verified!!");
+                          },
+                        );
+                      }
+                    },
+                    child: Center(
+                        child: Text(
+                      "VERIFY".toUpperCase(),
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    )),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
